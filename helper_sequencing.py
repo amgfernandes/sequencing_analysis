1c9861b6dce6f065eb0ed527abd59079cf5ce051
Miguel code <amgfernandes@gmail.com>
2020-12-15 16:45:10 +0100

    Revert "Work on helper functions>>>Batch load of multiple samples"
    This reverts commit 166de5bb9bbf65b21e0fd19b1464a0eb7ca446f0.

--- helper_sequencing.py
+++ helper_sequencing.py
@@ -1,24 +1,40 @@
+__authors__ = 'fernandes Dec 2020'
 #TODO finish helper class
+
+
 class Sequencing():
     """[summary]   
        Class with functions to help sequencing analysis
         """
-    def __init__(self, seqdata, stimuli, metadata):
+    def __init__(self, seqdata,metadata=None):
         """ Loads the experiment from sequencing data
         :param seqdata: sequencing data
-        :param metadata: metadata dictionary, for the sequencing data
+        :param metadata: metadata for the sequencing
         """
         self.seqdata = seqdata
         self.metadata = metadata
 
-    def return_indices_of_a(a, b):
+
+    def return_indices_not_in_list(self,a, b):
         b_set = set(b)
         return [i for i, v in enumerate(a) if v not in b_set]
 
-    def remove_genes(dgc_mat, gene_list):
+    def return_indices_inside_list(self,a, b):
+        b_set = set(b)
+        return [i for i, v in enumerate(a) if v in b_set]
+
+    def remove_gene_list(self,dgc_mat, gene_list):
     #Function receives a  gene matrix and a list of genes and returns a  gene matrix without the gene rows.
     #The loaded 10x data is a dgc matrix.
-        idx=return_indices_of_a(dgc_mat.var_names,gene_list.gene.values)
+        idx=self.return_indices_not_in_list(dgc_mat.var_names,gene_list.gene.values)
+        dgc_mat_corr= dgc_mat[:,idx]
+
+        return (dgc_mat_corr)
+    
+    def keep_gene_list(self,dgc_mat, gene_list):
+        #Function receives a  gene matrix and a list of genes and returns a  gene matrix without the gene rows.
+    #The loaded 10x data is a dgc matrix.
+        idx=self.return_indices_inside_list(dgc_mat.var_names,gene_list.gene.values)
         dgc_mat_corr= dgc_mat[:,idx]
 
         return (dgc_mat_corr)
